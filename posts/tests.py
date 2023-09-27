5f79af82428fd46ffaeb6189702c09c6617c582a
JacquesIversen <jackiracki1@gmail.com>
2023-09-27 22:56:11 +0000

    test tested once again

--- posts/tests.py
+++ posts/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from .models import Post
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 
 
 class PostListViewTests(APITestCase):
@@ -27,3 +27,44 @@ class PostListViewTests(APITestCase):
     def test_not_logged_in_user_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        adam = User.objects.create_user(username='adam', password='pass')
+        brian = User.objects.create_user(username='brian', password='pass')
+        Post.objects.create(
+            owner=adam, title='a title', content='adams content'
+        )
+        Post.objects.create(
+            owner=brian, title='brians title', content='brians content'
+        )
+
+    def test_can_retrieve_with_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_fetch_post_with_invalid_id(self):
+        # Attempt to fetch a post with an invalid ID (e.g., -1)
+        invalid_post_id = -1
+        invalid_post_url = f'/posts/{invalid_post_id}/'
+        
+        response = self.client.get(invalid_post_url)
+        
+        # Check that the response status code is 404 (Not Found)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_post_not_owned(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
