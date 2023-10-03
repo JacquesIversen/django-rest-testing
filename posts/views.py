a80dc255e510a8dc914d8d46cbec78fa25e7d528
JacquesIversen <jackiracki1@gmail.com>
2023-10-03 21:44:02 +0000

    adding filters

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -14,8 +15,20 @@ class PostList(generics.ListCreateAPIView):
         
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
     ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+
     ordering_fields = [
         'post_count',
         'followers_count',
@@ -32,8 +45,8 @@ class PostDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.annotate(
-        comments_count=Count('comment', distinct=True),
-        likes_count=Count('likes', distinct=True)
+        comments_count=Count('likes', distinct=True),
+        likes_count=Count('comment', distinct=True)
     ).order_by('-created_at')
 
 
