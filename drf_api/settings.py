413a270940f905e4801e8fae2ee59d56164cd945
JacquesIversen <jackiracki1@gmail.com>
2023-10-04 14:36:22 +0000

    allowed host problem

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,7 +67,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-jacquesiver-djangorestt-zesjek4o4ri.ws-eu105.gitpod.io', 'apirestji.herokuapp.com']
+ALLOWED_HOSTS = ['<apirestji>.herokuapp.com']
 
 
 # Application definition
